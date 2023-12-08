9a84eae25f12a08386eabb44cd1e745b9f7a14dd
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-08 09:04:55 +0900

    Title: 달에서 무게 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231208/달에서 무게 구하기/weight-on-the-moon.c
+++ 231208/달에서 무게 구하기/weight-on-the-moon.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    double weightOnEarth = 13.0;
-    double lunarGravity = 0.165;
-    double weightOnMoon = weightOnEarth * lunarGravity;
+    double 추의_무게 = 13;
+    double 달에서의_중력_비율 = 0.165;
 
-    printf("%.6lf * %.6lf = %.6lf\n", weightOnEarth, lunarGravity, weightOnMoon);
+    double 결과 = 추의_무게 * 달에서의_중력_비율;
+    printf("%.6lf * %.6lf = %.6lf\n", 추의_무게, 달에서의_중력_비율, 결과);
 
     return 0;
 }
