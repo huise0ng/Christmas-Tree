5b163e0850b6de7444c842955027174d5bdc1f53
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-10 22:10:51 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231210/데이터 교환/exchange-data.c
+++ 231210/데이터 교환/exchange-data.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int a = 5;
-    int b = 6;
-    int c = 7;
-    
-    printf("%d\n%d\n%d",c,b,a);
+    // 초기 값 설정
+    int a = 5, b = 6, c = 7;
+    int temp;
+
+    // 값 교환
+    temp = a;
+    a = c;
+    c = b;
+    b = temp;
+
+    // 출력
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+
     return 0;
 }
