d2dcb63cd8ebe6319273387c9c377c1befa70be5
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-11 01:06:36 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/세 실수의 반올림/rounding-of-three-actual-numbers.c
+++ 231211/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     double a, b, c;
-    scanf("%lf\n%lf\n%lf",&a,&b,&c);
-    printf(".3lf\n.3lf\n.3lf\n",a,b,c);
+    
+    // 세 개의 실수 입력 받기
+    scanf("%lf", &a);
+    scanf("%lf", &b);
+    scanf("%lf", &c);
+
+    // 입력 받은 실수들을 반올림하여 출력
+    printf("%.3lf\n", round(a * 1000) / 1000);
+    printf("%.3lf\n", round(b * 1000) / 1000);
+    printf("%.3lf\n", round(c * 1000) / 1000);
+
     return 0;
 }
