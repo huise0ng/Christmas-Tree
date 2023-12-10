e13af71f7b8319fd6c1b3b5a29bd3b90bd48488d
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-11 00:31:14 +0900

    Title: 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/입력받아 계산/input-calculate.c
+++ 231211/입력받아 계산/input-calculate.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
 int main() {
+    // 정수 a 입력 받기
     int a;
-    b = a + 2;
-    printf("%d\n",b);
+    scanf("%d", &a);
+
+    // a에 2를 더하여 계산
+    int result = a + 2;
+
+    // 결과 출력
+    printf("%d\n", result);
+
     return 0;
 }
