6ea922989e62fb84ebae6247f9352c9d4ecc73a4
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-11 10:23:29 +0900

    Title: 간단한 사칙연산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231211/간단한 사칙연산/simple-arithmetic-operation.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d",&a,&b);
+    int c = a+b;
+    int d = a-b;
+    int e = a/b;
+    int f = a % b;
+    printf("%d\%d\n%d\n%d",c,d,e,f);
+    return 0;
+}
