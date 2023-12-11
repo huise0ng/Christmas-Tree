685d434b9e5fffdba29dcf64270f6813f8021db4
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-11 12:11:01 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/합과 차의 나눗셈/divide-of-sum-and-sub.c
+++ 231211/합과 차의 나눗셈/divide-of-sum-and-sub.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int a, b;
-    scanf("%d %d",&a,&b);
-    int c = a+b;
-    int d = a - b;
-    int e = c % d;
-    printf("%d",d); 
+    double result;
+
+    // 두 정수 a, b 입력받기
+    scanf("%d %d", &a, &b);
+
+    // a + b를 a - b로 나눈 값을 계산
+    result = (double)(a + b) / (a - b);
+
+    // 결과를 소수점 둘째 자리까지 출력
+    printf("%.2lf\n", round(result * 100) / 100);
+
     return 0;
 }
