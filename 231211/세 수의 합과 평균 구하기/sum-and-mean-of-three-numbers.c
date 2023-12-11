65214f0f57ea1d1445f9c1e27e01909c11baaf33
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-11 13:56:13 +0900

    Title: 세 수의 합과 평균 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231211/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.c
+++ 231211/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.c
@@ -3,7 +3,7 @@
 int main() {
     int a, b, c;
     scanf("%d %d %d",&a,&b,&c);
-    printf("%d\n%.1lf", a + b + c, (double)(a + b+ c) / 3);
+    printf("%d\n%.0f", a + b + c, (double)(a + b+ c) / 3);
 
 
     return 0;
