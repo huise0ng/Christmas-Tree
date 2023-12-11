4958d3dd1b7ac513c7b32d37d68d85a2bbfa53c5
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-11 13:43:07 +0900

    Title: 전화번호 바꾸기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231211/전화번호 바꾸기/change-phone-number.c
+++ 231211/전화번호 바꾸기/change-phone-number.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    char = a[9];
-    char = b[9];
-    int c = 010;
-    scanf("%d-%s-%s",&c,&a,&b);
-    printf("%d-%d-%d",c,b,a);
+    // 전화번호 입력 받기
+    char phoneNumber[14];
+    scanf("%s", phoneNumber);
+
+    // 입력 받은 전화번호의 앞 4자리와 뒤 4자리를 바꾸기
+    char temp;
+    temp = phoneNumber[3];
+    phoneNumber[3] = phoneNumber[8];
+    phoneNumber[8] = temp;
+
+    temp = phoneNumber[4];
+    phoneNumber[4] = phoneNumber[9];
+    phoneNumber[9] = temp;
+
+    // 결과 출력
+    printf("%s\n", phoneNumber);
+
+    return 0;
 }
