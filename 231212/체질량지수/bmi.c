265cd3010f3236f9071bc8553d5230da8eae3db8
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-12 12:18:41 +0900

    Title: 체질량지수, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231212/체질량지수/bmi.c
+++ 231212/체질량지수/bmi.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    scanf("%d %d",&a,&b);
-    int c = b*b;
-    int d = a/c;
-    printf("%d\n",d);
-    if(a/c>=25){
-        printf("Obesity");
-    }
+    // 변수 선언
+    int h, w;
+
+	// 입력
+	scanf("%d %d", &h, &w);
+	
+	// 키(cm)에서 키(m)로 단위 환산을 한 뒤 
+	// 체질량지수 계산 식에 넣어야 함에 유의합니다.
+	int bmi = w * 100 * 100 / (h * h);
+    
+    // 출력
+	printf("%d\n", bmi);
+	if(bmi >= 25)
+		printf("Obesity");
     return 0;
 }
