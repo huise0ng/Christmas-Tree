34759273d49fd8fbdeb20ed86b09eb8bf41eeeb9
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-12 11:17:01 +0900

    Title: 전화번호 바꾸기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231212/전화번호 바꾸기/change-phone-number.c
+++ 231212/전화번호 바꾸기/change-phone-number.c
@@ -2,12 +2,18 @@
 
 int main() {
     // 전화번호 입력 받기
-    char phone_number[15]; // 끝에 null 문자('\0')를 저장할 공간을 추가
+    char phone_number[15];
     scanf("%s", phone_number);
 
     // 끝에 있는 '-'를 제거하여 앞 4자리와 뒤 4자리를 바꾸어 출력
-    phone_number[13] = '\0'; // 끝에 있는 '-'를 null 문자로 대체
-    printf("010-%c%c%c%c-%c%c%c%c\n", phone_number[8], phone_number[9], phone_number[10], phone_number[11], phone_number[4], phone_number[5], phone_number[6], phone_number[7]);
+    int len = strlen(phone_number);
+    if (phone_number[len - 1] == '-') {
+        phone_number[len - 1] = '\0';
+        len--;
+    }
+    
+    printf("010-%c%c%c%c-%c%c%c%c\n", phone_number[len - 4], phone_number[len - 3], phone_number[len - 2], phone_number[len - 1],
+                                       phone_number[len - 8], phone_number[len - 7], phone_number[len - 6], phone_number[len - 5]);
 
     return 0;
 }
