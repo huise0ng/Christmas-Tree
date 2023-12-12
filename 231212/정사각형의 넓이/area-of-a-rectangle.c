1340dd4bf06406afe56f8ab15144c7063502456f
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-12 12:23:18 +0900

    Title: 정사각형의 넓이, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231212/정사각형의 넓이/area-of-a-rectangle.c
+++ 231212/정사각형의 넓이/area-of-a-rectangle.c
@@ -4,9 +4,9 @@ int main() {
     int a;
     scanf("%d",&a);
     int b = a*a;
-    printf("%d",b);
+    printf("%d\n",b);
 
-    if(a<=5){
+    if(a<5){
         printf("tiny");
     }
     return 0;
