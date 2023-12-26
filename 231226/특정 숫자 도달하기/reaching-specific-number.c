39728a5cff1fc9bc848417b186dba25105a65de0
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2023-12-26 14:04:55 +0900

    Title: 특정 숫자 도달하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231226/특정 숫자 도달하기/reaching-specific-number.c
+++ 231226/특정 숫자 도달하기/reaching-specific-number.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
 int main() {
+    // 변수 선언
     int arr[10];
-    int a = 0;
-    int b = 0;
-
-    for(int i = 0; i<10; i++){
-        scanf("%d", &arr[10]);
-
-    }
+    int sum = 0;
+    int cnt = 0;
+	
+	// 10개의 정수를 입력받습니다.
     for(int i = 0; i < 10; i++) {
-        if(arr[i] >= 250)
-            break;
-        a += arr[i];
-        b++;
+        scanf("%d", &arr[i]);
     }
 
+	// 10개의 정수 중 250 이상의 수가 나올 때 까지의 수의 합계와 개수를 구합니다.
+	for(int i = 0; i < 10; i++) {
+        if(arr[i] >= 250) 
+			break;
+        sum += arr[i];
+        cnt++;
+    }
+	
+    // 평균을 구합니다.
+    double avg = (double)sum / cnt;
 
-    double avg = (double)a/b;
+    // 출력
+    printf("%d %.1lf", sum, avg);
 
-    printf("%d %1lf", a,b);
-    
     return 0;
 }
