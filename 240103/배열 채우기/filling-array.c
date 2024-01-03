96678f19587f2e70b93722667c855893c9e8af53
Huiseong Shin <128358820+huise0ng@users.noreply.github.com>
2024-01-03 15:34:41 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/배열 채우기/filling-array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    
+    int count = 0;
+
+    for(int i = 0; i< 10; i++){
+        scanf("%d",&arr[i]);
+
+        if(arr[i]==0){
+            break;
+        }
+        ++count;
+    }
+    for(int i = count - 1; i >= 0; i--){
+        printf("%d ",arr[i]);
+    }
+
+}
